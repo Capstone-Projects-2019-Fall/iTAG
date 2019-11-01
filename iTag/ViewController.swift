2f861c7c420146c86bd54c6f0f7b8c0dafc191fc
Leo Vergnetti <tuj52136@temple.edu>
2019-11-01 11:04:03 -0400

    Add highlight textview to project

--- iTag/ViewController.swift
+++ iTag/ViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var highlightView: UITextView!
+    
+    var text = "Hi There Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        highlightView.text = text
+        
     }
 
 
