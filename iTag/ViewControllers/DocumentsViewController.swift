95fe760c7ba4c2e42f353a94ae4c073a1e9f9006
Leo Vergnetti <tuj52136@temple.edu>
2019-11-12 11:52:27 -0500

    Add tableview and DocumentPicker to Document window

--- iTag/ViewControllers/DocumentsViewController.swift
+++ iTag/ViewControllers/DocumentsViewController.swift
@@ -7,24 +7,50 @@
 //
 
 import UIKit
+import MobileCoreServices
 
-class DocumentsViewController: UIViewController {
 
+class DocumentsViewController: UIViewController, UIDocumentPickerDelegate, UITableViewDataSource, UITableViewDelegate{
+
+    
+    @IBOutlet weak var documentsTableView: UITableView!
+    
+    let documents = ["First Document", "Second Document", "Third Document"]
+    
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        documentsTableView.delegate = self
+        documentsTableView.dataSource = self
+        
+        openDocumentPicker()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+        print(urls)
     }
-    */
+    
+    
+    func openDocumentPicker() {
+        // Do any additional setup after loading the view.
+        let types: [String] = [kUTTypePDF as String]
+        let documentPicker = UIDocumentPickerViewController(documentTypes: types, in: .import)
+        documentPicker.delegate = self
+        documentPicker.modalPresentationStyle = .formSheet
+        self.present(documentPicker, animated: true, completion: nil)
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return documents.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DocumentCell") as! DocumentCell
+        cell.titleLabel.text = documents[indexPath.row]
+        return cell
+    }
 
 }
---
+++ iTag/Views/DocumentCell.swift
@@ -0,0 +1,26 @@
+//
+//  DocumentCell.swift
+//  iTag
+//
+//  Created by Leo Vergnetti on 11/12/19.
+//  Copyright © 2019 Leo Vergnetti. All rights reserved.
+//
+
+import UIKit
+
+class DocumentCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
